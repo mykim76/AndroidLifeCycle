e3701cdb0d708573eaac3af8033370c16005f477
mykim76 <iyoungfm@gmail.com>
2020-06-04 20:17:49 +0900

    onResume()까지 실행

--- app/src/main/java/com/example/androidlifecycle/MainActivity.kt
+++ app/src/main/java/com/example/androidlifecycle/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.androidlifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("main screen", "Excuting onCreate")
+
+        Log.d("메인화면","onCreate 실행됨")
+        btnGoToOther.setOnClickListener {
+
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //onCreate 가 실행 된 후 onResume 실행 함
+        //화면 이동 후 돌아올 때 onCreate는 실행되지 않지만 onResume을 실행 함=>가려졌다가 다시 나타날 때 실행됨
+        Log.d("메인화면","onResume 실행됨")
     }
 }
---
+++ app/src/main/java/com/example/androidlifecycle/OtherActivity.kt
@@ -0,0 +1,20 @@
+package com.example.androidlifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_other.*
+
+class OtherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+        Log.d("다른화면","onCreate 실행됨")
+        btnGoBack.setOnClickListener {
+
+            finish()
+        }
+    }
+}
